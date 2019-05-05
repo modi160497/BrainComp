6df313acefd2ff6ecf02704c3b697c54ecd18751
modi160497 <30540253+modi160497@users.noreply.github.com>
2019-05-05 01:13:55 -0400

    dataset

--- snn.py
+++ snn.py
@@ -21,20 +21,24 @@ if __name__ == '__main__':
 
     stop_words = set(stopwords.words('english'))
 
+    length = []
+
     for i in range(0, len(sentences)):
 
         data = tokenizer.tokenize(sentences[i][1])
         filtered_sentence = [w for w in data if not w in stop_words] # remove articles and stop words
         words = [word for word in filtered_sentence if word.isalpha()]
+        length.append(len(words))
         tokens.append(words)
 
     print(tokens[0])
+    print(max(length)) # max length of the setence used
 
     sen_w2v = Word2Vec(size= 200, min_count=8)
     sen_w2v.build_vocab(tokens)
     sen_w2v.train(tokens, total_examples=len(tokens), epochs=10)
 
-    print(sen_w2v.most_similar('accident'))
+    print(sen_w2v.most_similar('love'))
 
 
 
